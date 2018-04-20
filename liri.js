fdde3390119e6a0bfe750d4057f836701b1e0df0
Alex Trilling <atrill90@gmail.com>
2018-04-20 19:48:52 -0400

    no idea why this worked but ok

--- liri.js
+++ liri.js
@@ -1,2 +1,39 @@
 require("dotenv").config();
-require("keys.js");
+
+
+
+let keys = require("./keys.js");
+// let Spotfiy = require('node-spotify-api');
+let Twitter = require('twitter');
+let fs = require("fs");
+let request = require("request");
+// let spotify = new Spotify(keys.spotify);
+let client = new Twitter(keys.twitter);
+
+nodeArg = process.argv;
+let userInput = process.argv[2];
+
+
+// for (let i =3; nodeArg.length; i++){
+//     nodeArg += process.argv[i] + "+";
+// }
+myTweets();
+// switch (userInput) {
+//     case `my-tweets`:
+//         myTweets();
+//         break;
+//     case `spotify-this-song`:
+//         spotifyThis();
+//         break;
+//     case `movie-this`:
+//         movieThis();
+//         break;
+//     case `do-what-it-says`:
+//         justDoIt();      
+// }
+
+function myTweets(){
+    client.get('statuses/user_timeline', function(error, tweets, response) {
+        console.log((tweets));
+     });
+}
